f2765c22181bf76d70c0f30fb6ecbc08f26afafa
Pierre Obermaier <pierre.obermaier@googlemail.com>
2025-07-09 15:54:45 +0200

    B Fixing metrics

--- src/metrics.py
+++ src/metrics.py
@@ -31,15 +31,20 @@ class Metrics:
         latest_predictions = pred_bio_runs[-1]
         for i, true_label in enumerate(true_bio):
             if latest_predictions[i] == true_label:
-                if latest_predictions[i] != 'O':
-                    self.true_positives[true_label] += 1
-                else:
+                if true_label == 'O':
+                    # y and y_hat are 'O'
                     self.true_negatives[true_label] += 1
-            else:
-                if latest_predictions[i] != 'O':
-                    self.false_positives[latest_predictions[i]] += 1
                 else:
+                    # y and y_hat are same NER
+                    self.true_positives[true_label] += 1
+            else:
+                if latest_predictions[i] == 'O': # false negative
+                    # y is a NER and y_hat is 'O'
                     self.false_negatives[true_label] += 1
+                else: # false positive
+                    # y is a 'O' or NER X and y_hat is a NER Y
+                    self.false_positives[true_label] += 1
+
         # If the latest prediction run is shorter than the true BIO tags, we have false negatives unless there are no further entities
         if len(true_bio) > len(latest_predictions):
             for i in range(len(latest_predictions), len(true_bio)):
@@ -55,15 +60,25 @@ class Metrics:
         # Print table with entity types and their counts
         print(f"{'Entity Type':<20} {'TP':<10} {'TN':<10} {'FP':<10} {'FN':<10}")
         print("-" * 60)
-        entity_types = list(ENTITY_MAP.values())
-        # NOTE: dslim/bert-base-NER throws some NER categories into a MISC bucket instead of their specific categories
-        #       so we add MISC to the list of entity types to ensure we print it
-        entity_types.extend(['B-MISC', 'I-MISC'])
-        for entity_type in entity_types:
+        for entity_type in list(ENTITY_MAP.values()):
             tp = self.true_positives[entity_type]
             tn = self.true_negatives[entity_type]
             fp = self.false_positives[entity_type]
             fn = self.false_negatives[entity_type]
+            
+            if entity_type == 'O':
+                # if true entity is 'O' (negative) -> there cannot be true positives or false negatives
+                tp = self._format_highlight(str(tp)) if tp > 0 else "N/A"
+                fn = self._format_highlight(str(fn)) if fn > 0 else "N/A"
+            else:
+                # if true entity is not 'O' (positive) -> there cannot be true negatives
+                tn = self._format_highlight(str(tn)) if tn > 0 else "N/A"
+            
             print(f"{entity_type:<20} {tp:<10} {tn:<10} {fp:<10} {fn:<10}")
         print(f"{'Total':<20} {sum(self.true_positives.values()):<10} {sum(self.true_negatives.values()):<10} {sum(self.false_positives.values()):<10} {sum(self.false_negatives.values()):<10}")
         print("-" * 60)
+
+    @staticmethod
+    def _format_highlight(text: str) -> str:
+        """Highlight the text in a way that is suitable for printing."""
+        return f"*{text}*?!"
